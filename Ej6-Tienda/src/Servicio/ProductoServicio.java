e83a783c2bb2c5d49d41391f927449ce4ca8822d
Brennsel <brennsell@gmail.com>
2023-05-18 22:02:21 -0300

    Se agregaron ej 5 y ej 6

--- Ej6-Tienda/src/Servicio/ProductoServicio.java
+++ Ej6-Tienda/src/Servicio/ProductoServicio.java
@@ -2,8 +2,8 @@
 Se necesita una aplicación para una tienda en la cual queremos almacenar los distintos
 productos que venderemos y el precio que tendrán. Además, se necesita que la
 aplicación cuente con las funciones básicas.
-Estas las realizaremos en el servicio. Como, introducir un elemento, modificar su precio,
-eliminar un producto y mostrar los productos que tenemos con su precio (Utilizar
+Estas las realizaremos en el servicio. Como, introducir un elementoV, modificar su precioV,
+eliminar un producto y mostrar los productosV que tenemos con su precio (Utilizar
 Hashmap). El HashMap tendrá de llave el nombre del producto y de valor el precio.
 Realizar un menú para lograr todas las acciones previamente mencionadas.
  */
@@ -11,21 +11,18 @@ Realizar un menú para lograr todas las acciones previamente mencionadas.
 package Servicio;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Scanner;
 
 public class ProductoServicio {
 
     //Producto prod = new Producto();
     HashMap<String, Double> mapProductos = new HashMap<>();
-    Scanner leer = new Scanner(System.in).useDelimiter("/n");
+    Scanner leer = new Scanner(System.in).useDelimiter("\n");
 
     public void ingresarProductos(){
 
-        boolean continuar = false, esIncorrecto;
         String nombre;
         Double precio;
-        int opc;
 
         do {
             System.out.println("Ingreso de producto");
@@ -37,51 +34,82 @@ public class ProductoServicio {
 
             mapProductos.put(nombre, precio);
 
-            do {
-                System.out.println("--------------------------------------------------------------");
-                System.out.println("Desea continuar ingresando?");
-                System.out.println("1. SI");
-                System.out.println("2. NO");
-                System.out.println("--------------------------------------------------------------");
-                opc = leer.nextInt();
+        } while (menuContinuar());
+    }
 
-                if(opc == 1){
-                    continuar= true;
-                    esIncorrecto = false;
-                }else if(opc==2){
-                    continuar = false;
-                    esIncorrecto = false;
-                } else{
-                    System.out.println("Ingreso incorrecto.");
-                    esIncorrecto = true;
-                }
-            }while(esIncorrecto);
+    public Boolean menuContinuar(){
 
-        } while (continuar);
+        Integer opc;
+        Boolean continuar, esIncorrecto;
 
+        do {
+            continuar = false;
+            esIncorrecto = false;
 
+            System.out.println("--------------------------------------------------------------");
+            System.out.println("Desea continuar ingresando?");
+            System.out.println("1. SI");
+            System.out.println("2. NO");
+            System.out.println("--------------------------------------------------------------");
+            opc = leer.nextInt();
+
+            if(opc == 1){
+                continuar= true;
+            } else if(opc!=2){
+                System.out.println("Ingreso incorrecto.");
+                esIncorrecto = true;
+            }
+        }while(esIncorrecto);
+
+        return continuar;
     }
 
     public void modificarPrecio(){
 
         String nombre;
-        Boolean seEncontro;
+        Double precio;
 
         System.out.println("--------------------------------------------------------------");
-        System.out.println("Ingrese el producto del que desea modificar el precio: ");
+        System.out.print("Ingrese el producto del que desea modificar el precio: ");
+        nombre = leer.next();
 
-        do{
-            nombre = leer.next();
+        if(mapProductos.containsKey(nombre)){
 
-            while ()
+            System.out.print("Ingrese el nuevo precio: $ ");
+            precio = leer.nextDouble();
 
+            mapProductos.put(nombre, precio);
 
-        }while(!seEncontro);
-
-
-
-
+        } else{
+            System.out.println("No se encontro el producto.");
+        }
     }
+
+    public void mostrarProductos(){
+
+        System.out.println("--------------------------------------------------------------");
+        System.out.println("Lista de productos: ");
+
+        for(String llave : mapProductos.keySet()){ //devuleve la lista con todas las llaves
+            System.out.println("Producto: " +llave + " ------> Precio $" +mapProductos.get(llave));
+        }
+    }
+
+    public void eliminarProducto(){
+
+        String producto;
+
+        System.out.println("--------------------------------------------------------------");
+        System.out.println("Ingrese el producto a eliminar: ");
+        producto = leer.next();
+
+        if(mapProductos.containsKey(producto)){
+            mapProductos.remove(producto);
+        } else {
+            System.out.println("El producto no se encuentra.");
+        }
+    }
+
 
 
 
--- Ej6-Tienda/src/TiendaMain/Main.java
+++ Ej6-Tienda/src/TiendaMain/Main.java
@@ -1,13 +1,23 @@
 package TiendaMain;
 
 import Servicio.ProductoServicio;
-
 public class Main {
     public static void main(String[] args) {
 
         ProductoServicio ps = new ProductoServicio();
 
+        System.out.println("======TIENDA===MENU======");
+        System.out.println(" 1. INGRESAR PRODUCTO");
+        System.out.println(" 2. MODIFICAR PRECIO");
+        System.out.println(" 3. ELIMINAR PRODUCTOS");
+        System.out.println(" 4. MOSTRAR PRODUCTOS");
+
         ps.ingresarProductos();
+        ps.mostrarProductos();
+        ps.modificarPrecio();
+        ps.mostrarProductos();
+        ps.eliminarProducto();
+
 
 
 
